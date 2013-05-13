09802a01c2eae3b61abe2fe0f07bc17b68d9b98f
Salman Ahmad <salman@salmanahmad.com>
2013-05-13 03:37:55 -0400

    Saving is now working.

--- examples/test.js
+++ examples/test.js
@@ -1,8 +1,13 @@
 
 var TrailMix = require("../lib/trailmix")
 
-var database = new TrailMix.Database("sqlite")
-
-database.save("edit", {}, {
-  a: "o"
+var database = new TrailMix.Database("sqlite", {}, function() {
+  database.save("initial", {}, {"hello": "World!"}, function() {
+    console.log("saved!")
+    
+    database.load(["hello"], function(values) {
+      console.log("Load returned this values: " + JSON.stringify(values));
+    })
+    
+  })
 })
